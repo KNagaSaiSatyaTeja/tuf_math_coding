a63033ebf4fb4d10b52ca32aa253ee2631926f11
Naga sai Satya Teja <nagasai.koli@gmail.com>
2025-03-05 20:25:49 +0530

    modified missing number

--- assigment/array/missing-element-worst-time-complexity.cpp
+++ assigment/array/missing-element-worst-time-complexity.cpp
@@ -3,18 +3,19 @@ using namespace std;
 
 int missing(vector<int> &nums)
 {
-
+    int j = 0;
     sort(nums.begin(), nums.end());
 
     if (nums[0] != 0)
         return 0;
 
-    for (int i = 0; i < nums.size() - 1; i++)
+    for (int i = 0; i < nums.size(); i++)
     {
-        if (nums[i] + 1 != nums[i + 1])
+        if (nums[i] + 1 != nums[j])
         {
             return nums[i] + 1;
         }
+        j++;
     }
 
     return nums.size();
