9f12ef4a952f1a2b3dd0885d92057f22f813af02
Naga sai Satya Teja <nagasai.koli@gmail.com>
2025-05-29 11:07:08 +0530

    bubble sort completed

---
+++ assigment/array/sorting/Bubble_sort.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void selection(vector<int> &nums)
+{
+    for (int i = 0; i < nums.size(); i++)
+    {
+
+        for (int j = 0; j < nums.size() - 1; j++)
+        {
+            if (nums[j] > nums[j + 1])
+            {
+                swap(nums[j], nums[j + 1]);
+            }
+        }
+    }
+}
+
+int main()
+{
+    vector<int> nums = {7, 4, 1, 2, 9};
+    selection(nums);
+    for (auto i : nums)
+    {
+        cout << i << " ";
+    }
+    return 0;
+}
